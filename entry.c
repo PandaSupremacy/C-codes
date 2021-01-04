ca7e5e707eb8ca30c6cd5fcc0ab21370ce4ba2dc
PreetibarnaPanda <72810835+PreetibarnaPanda@users.noreply.github.com>
2021-01-04 21:37:58 +0530

    added just a fun parody program

---
+++ entry.c
@@ -0,0 +1,19 @@
+/*I have my exams the next day and I am doin this... nevermind its programming in c*/
+//yes I could have used a single line comment too but MY CODE
+voidmain()
+{
+	printf("hello world you still have covid");
+	printf("enter the number of relationships u have had till now");
+	int rel;
+	scanf("%d",&rel);
+	if(rel==0)
+	printf("ohh Hi Samuel Miller or Small pp person");
+	if(rel>=1&&rel<=3)
+	printf("Ahh poor heartbroken boi");
+	if(rel>3)
+	printf("You ain't ever getting married dude");
+	if(rel<0)
+	printf("man u high on cocaine");
+	
+	
+}
